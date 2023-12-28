7f76627ed44d7dc821a7fc5bf4c133ee687c42a8
johnbean393 <johnbean393@gmail.com>
2023-12-28 13:06:26 +0800

    Update Double.swift

--- Sources/PattoniumKit/Extensions/Double.swift
+++ Sources/PattoniumKit/Extensions/Double.swift
@@ -10,13 +10,19 @@ import Foundation
 extension Double {
 	
 	// Add zeros to a double
-	public func addZeros(wholeNumTarget: Int, fractionalTarget: Int) -> String {
+	public func addZeros(wholeNumTarget: Int = -1, fractionalTarget: Int = -1) -> String {
 		// Get whole number with leading zeros
 		let wholeNum: Int = Int(self)
-		let wholeNumStr: String = String(repeating: "0", count: wholeNumTarget - String(wholeNum).count) + String(wholeNum)
+		var wholeNumStr: String = String(wholeNum)
+		if wholeNumTarget != -1 {
+			wholeNumStr = String(repeating: "0", count: wholeNumTarget - String(wholeNum).count) + String(wholeNum)
+		}
 		// Get fraction with leading zeros
 		let rawFractionalStr: String = String(self.description.split(separator: ".").last ?? "0")
-		let fractionalStr: String = rawFractionalStr + String(repeating: "0", count: fractionalTarget - rawFractionalStr.count)
+		var fractionalStr: String = rawFractionalStr
+		if fractionalTarget != -1 {
+			fractionalStr = rawFractionalStr + String(repeating: "0", count: fractionalTarget - rawFractionalStr.count)
+		}
 		return "\(wholeNumStr).\(fractionalStr)"
 	}
 	
