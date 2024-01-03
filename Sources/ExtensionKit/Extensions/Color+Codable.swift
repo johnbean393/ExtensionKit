c691736802688060a93160073aa9ca7a57dd6198
johnbean393 <johnbean393@gmail.com>
2024-01-03 14:29:34 +0800

    Update Color+Codable.swift

--- Sources/ExtensionKit/Extensions/Color+Codable.swift
+++ Sources/ExtensionKit/Extensions/Color+Codable.swift
@@ -7,91 +7,81 @@
 
 import SwiftUI
 
-extension Color: Codable {
-	init(hex: String) {
-		let rgba = hex.toRGBA()
-		
-		self.init(.sRGB,
-				  red: Double(rgba.r),
-				  green: Double(rgba.g),
-				  blue: Double(rgba.b),
-				  opacity: Double(rgba.alpha))
-	}
-	
-	public init(from decoder: Decoder) throws {
-		let container = try decoder.singleValueContainer()
-		let hex = try container.decode(String.self)
-		
-		self.init(hex: hex)
-	}
-	
-	public func encode(to encoder: Encoder) throws {
-		var container = encoder.singleValueContainer()
-		try container.encode(toHex)
-	}
-	
-	var toHex: String? {
-		return toHex()
-	}
-	
-	func toHex(alpha: Bool = false) -> String? {
-		guard let components = cgColor?.components, components.count >= 3 else {
-			return nil
+extension Color {
+	static func fromString(_ colorStr: String) -> Color {
+		/***
+		 Meant to be used as a return to Color after storing a color using Color.description
+		 from Color.'enum'.description - for example Color.pink.description = "pink"
+		 or if you assign a 'var' to Color.pink, var.description = "pink"
+		 -or-
+		 from Color.description format of "#xxxxxxxx"
+		 -or-
+		 from a return of ColorPicker with the following starting values split by spaces
+		 UIExtendedSRGBColorSpace (5 values)
+		 kCGColorSpaceModelMonochrome (3 values)
+		 kCGColorSpaceModelRGB (5 values)
+		 kCGColorSpaceModelCMYK (6 values)
+		 where values are separated by spaces
+		 */
+		var red = 0.8, green = 0.8, blue = 0.8, opacity = 1.0 //defaults to gray on errors
+		switch colorStr {
+				//Color enums
+			case "black": return Color.black
+			case "blue": return Color.blue
+			case "brown": return Color.brown
+			case "clear": return Color.clear
+			case "cyan": return Color.cyan
+			case "gray": return Color.gray
+			case "green": return Color.green
+			case "indigo": return Color.indigo
+			case "mint": return Color.mint
+			case "orange": return Color.orange
+			case "pink": return Color.pink
+			case "purple": return Color.purple
+			case "red": return Color.red
+			case "teal": return Color.teal
+			case "white": return Color.white
+			case "yellow": return Color.yellow
+			default:
+				
+				if (colorStr.count == 9) && (colorStr.first == "#") {
+					//Color.description from Color(red:green:blue:opacity)
+					red = Double(UInt32(String(Array(colorStr)[1...2]), radix: 16) ?? 0) / 255.0
+					green = Double(UInt32(String(Array(colorStr)[3...4]), radix: 16) ?? 0) / 255.0
+					blue = Double(UInt32(String(Array(colorStr)[5...6]), radix: 16) ?? 0) / 255.0
+					opacity = Double(UInt32(String(Array(colorStr)[7...8]), radix: 16) ?? 0) / 255.0
+				} else {
+					let values = colorStr.split(separator: " ")
+					if values.count == 6 {
+						//CMYK - Not an exact match, but ok for choosing control colors
+						let cyan = (1.0 - (Double(values[1]) ?? 0.0))
+						let magenta = (1.0 - (Double(values[2]) ?? 0.0))
+						let yellow = (1.0 - (Double(values[3]) ?? 0.0))
+						let black = (1.0 - (Double(values[4]) ?? 0.0))
+						red = (cyan * black)
+						green = (magenta * black)
+						blue = (yellow * black)
+						opacity = (Double(values[5]) ?? 0.0)
+					} else {
+						if values.count == 3 {
+							//Monochrome
+							red = (Double(values[1]) ?? 0.0)
+							green = red
+							blue = red
+							opacity = (Double(values[2]) ?? 0.0)
+						} else {
+							if values.count == 5 {
+								//RGB
+								red = (Double(values[1]) ?? 0.0)
+								green = (Double(values[2]) ?? 0.0)
+								blue = (Double(values[3]) ?? 0.0)
+								opacity = (Double(values[4]) ?? 0.0)
+							}
+						}
+					}
+				}
+				return Color(red: red, green: green, blue: blue, opacity: opacity)
+				
 		}
-		
-		let r = Float(components[0])
-		let g = Float(components[1])
-		let b = Float(components[2])
-		var a = Float(1.0)
-		
-		if components.count >= 4 {
-			a = Float(components[3])
-		}
-		
-		if alpha {
-			return String(format: "%02lX%02lX%02lX%02lX",
-						  lroundf(r * 255),
-						  lroundf(g * 255),
-						  lroundf(b * 255),
-						  lroundf(a * 255))
-		}
-		else {
-			return String(format: "%02lX%02lX%02lX",
-						  lroundf(r * 255),
-						  lroundf(g * 255),
-						  lroundf(b * 255))
-		}
-	}
-}
-
-extension String {
-	func toRGBA() -> (r: CGFloat, g: CGFloat, b: CGFloat, alpha: CGFloat) {
-		var hexSanitized = self.trimmingCharacters(in: .whitespacesAndNewlines)
-		hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-		
-		var rgb: UInt64 = 0
-		
-		var r: CGFloat = 0.0
-		var g: CGFloat = 0.0
-		var b: CGFloat = 0.0
-		var a: CGFloat = 1.0
-		
-		let length = hexSanitized.count
-		
-		Scanner(string: hexSanitized).scanHexInt64(&rgb)
-		
-		if length == 6 {
-			r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-			g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-			b = CGFloat(rgb & 0x0000FF) / 255.0
-		}
-		else if length == 8 {
-			r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-			g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-			b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-			a = CGFloat(rgb & 0x000000FF) / 255.0
-		}
-		
-		return (r, g, b, a)
 	}
 }
