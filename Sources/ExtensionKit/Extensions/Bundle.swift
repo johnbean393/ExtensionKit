b703c22f2ee55f42c1e696834cf41abf67beb4f8
johnbean393 <johnbean393@gmail.com>
2024-01-03 12:19:39 +0800

    Added data model class

---
+++ Sources/ExtensionKit/Extensions/Bundle.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle.swift
+//
+//
+//  Created by Bean John on 3/1/2024.
+//
+
+import Foundation
+
+extension Bundle {
+	
+	// Application name shown under the application icon.
+	var applicationName: String? {
+		object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ??
+		object(forInfoDictionaryKey: "CFBundleName") as? String
+	}
+	
+}
---
+++ Sources/ExtensionKit/Tools/DataModel.swift
@@ -0,0 +1,118 @@
+//
+//  DataModel.swift
+//
+//
+//  Created by Bean John on 3/1/2024.
+//
+
+import Foundation
+import SwiftUI
+
+public class ValueDataModel<Value>: ObservableObject where Value: Codable & Equatable {
+	
+	init(appDirName: String = Bundle.main.applicationName ?? Bundle.main.description, datastoreName: String = "\(Bundle.main.applicationName ?? Bundle.main.description)Data") {
+		// Set path names
+		self.appDirName = appDirName
+		self.datastoreName = datastoreName
+		// Init methods
+		checkIfAppDirExists()
+		checkIfDataStoreExists()
+		load()
+	}
+	
+	private var appDirName: String
+	private var datastoreName: String
+//	static var shared: ValueDataModel = ValueDataModel()
+	@Published var values: [Value] = [] {
+		
+		// Save after change
+		didSet {
+			save()
+		}
+		
+	}
+	
+	func checkIfAppDirExists() {
+		// Get application directory
+		let appSupportUrl: URL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+		let appDirUrl: URL = appSupportUrl.appendingPathComponent(appDirName)
+		let appDirPath: String = appDirUrl.posixPath()
+		// If app dir does not exist
+		if !FileManager.default.fileExists(atPath: appDirPath) {
+			// Make dir
+			do {
+				try FileManager.default.createDirectory(at: appDirUrl, withIntermediateDirectories: true)
+			} catch {
+				// Failed to make dir, throw fatal error
+				fatalError("Failed to initialize application directory.")
+			}
+		}
+	}
+	
+	func checkIfDataStoreExists() {
+		// Get datastore url
+		let datastorePath: String = getDataStoreUrl().posixPath()
+		// If app dir does not exist
+		if !FileManager.default.fileExists(atPath: datastorePath) {
+			// Make datastore by saving blank data
+			values = []
+			save()
+		}
+	}
+	
+	func getAppDirUrl() -> URL {
+		// Get application directory
+		let appSupportUrl: URL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+		let appDirUrl: URL = appSupportUrl.appendingPathComponent(appDirName)
+		// Return url after checks passed
+		return appDirUrl
+	}
+	
+	func getDataStoreUrl() -> URL {
+		return getAppDirUrl().appendingPathComponent("\(datastoreName).json")
+	}
+	
+	public func load() {
+		// Get datastore url
+		let dataStoreUrl: URL = getDataStoreUrl()
+		do {
+			// Load data
+			let rawData: Data = try Data(contentsOf: dataStoreUrl, options: .mappedIfSafe)
+			let rawValues: [Value] = try JSONDecoder().decode([Value].self, from: rawData)
+			values = rawValues
+		} catch {
+			print("Failed to load data")
+		}
+	}
+	
+	public func save() {
+		// Get datastore url
+		let dataStoreUrl: URL = getDataStoreUrl()
+		do {
+			// Save data
+			let rawData: Data = try JSONEncoder().encode(values)
+			try rawData.write(to: dataStoreUrl, options: .atomic)
+		} catch {
+			print("Failed to save data")
+		}
+	}
+	
+	public func delete(_ value: Binding<Value>) {
+		withAnimation(.spring()) {
+			values = values.filter { $0 != value as! Value }
+		}
+	}
+	
+	public func delete(_ value: Value) {
+		withAnimation(.spring()) {
+			values = values.filter { $0 != value }
+		}
+	}
+	
+	public func add(_ value: Value) {
+		withAnimation(.spring()) {
+			values.append(value)
+		}
+	}
+	
+}
