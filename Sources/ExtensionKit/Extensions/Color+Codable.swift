89c825d99619231d0d8be84059e0ce8f52978310
johnbean393 <johnbean393@gmail.com>
2024-01-07 13:50:10 +0800

    Update Color+Codable.swift

--- Sources/ExtensionKit/Extensions/Color+Codable.swift
+++ Sources/ExtensionKit/Extensions/Color+Codable.swift
@@ -8,40 +8,64 @@
 import SwiftUI
 
 #if os(iOS)
-typealias PlatformColor = UIColor
-extension Color {
-	init(platformColor: PlatformColor) {
-		self.init(uiColor: platformColor)
-	}
-}
+import UIKit
+#elseif os(watchOS)
+import WatchKit
 #elseif os(macOS)
-typealias PlatformColor = NSColor
-extension Color {
-	init(platformColor: PlatformColor) {
-		self.init(nsColor: platformColor)
-	}
-}
+import AppKit
 #endif
 
-let color = Color(.sRGB, red: 0, green: 0, blue: 1, opacity: 1)
-
-func encodeColor() throws -> Data {
-	let platformColor = PlatformColor(color)
-	return try NSKeyedArchiver.archivedData(
-		withRootObject: platformColor,
-		requiringSecureCoding: true
-	)
-}
-
-func decodeColor(from data: Data) throws -> Color {
-	guard let platformColor = try NSKeyedUnarchiver
-		.unarchiveTopLevelObjectWithData(data) as? PlatformColor
-	else {
-		throw DecodingError.wrongType
+fileprivate extension Color {
+#if os(macOS)
+	typealias SystemColor = NSColor
+#else
+	typealias SystemColor = UIColor
+#endif
+	
+	var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
+		var r: CGFloat = 0
+		var g: CGFloat = 0
+		var b: CGFloat = 0
+		var a: CGFloat = 0
+		
+#if os(macOS)
+		SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+		// Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
+#else
+		guard SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else {
+			// Pay attention that the color should be convertible into RGB format
+			// Colors using hue, saturation and brightness won't work
+			return nil
+		}
+#endif
+		
+		return (r, g, b, a)
 	}
-	return Color(platformColor: platformColor)
 }
 
-enum DecodingError: Error {
-	case wrongType
+extension Color: Codable {
+	enum CodingKeys: String, CodingKey {
+		case red, green, blue
+	}
+	
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let r = try container.decode(Double.self, forKey: .red)
+		let g = try container.decode(Double.self, forKey: .green)
+		let b = try container.decode(Double.self, forKey: .blue)
+		
+		self.init(red: r, green: g, blue: b)
+	}
+	
+	public func encode(to encoder: Encoder) throws {
+		guard let colorComponents = self.colorComponents else {
+			return
+		}
+		
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		
+		try container.encode(colorComponents.red, forKey: .red)
+		try container.encode(colorComponents.green, forKey: .green)
+		try container.encode(colorComponents.blue, forKey: .blue)
+	}
 }
