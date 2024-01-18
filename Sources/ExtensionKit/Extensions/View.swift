0c881feba9ffb4ac7c4fae0a7fc81658982ef444
johnbean393 <johnbean393@gmail.com>
2024-01-18 11:27:13 +0800

    Update View.swift

--- Sources/ExtensionKit/Extensions/View.swift
+++ Sources/ExtensionKit/Extensions/View.swift
@@ -24,4 +24,17 @@ extension View {
 			.shadow(color: color, radius: radius / 3)
 	}
 	
+	public func innerShadow<S: Shape>(using shape: S, angle: Angle = .degrees(0), color: Color = .black, width: CGFloat = 6, blur: CGFloat = 6) -> some View {
+		let finalX = CGFloat(cos(angle.radians - .pi / 2))
+		let finalY = CGFloat(sin(angle.radians - .pi / 2))
+		return self
+			.overlay(
+				shape
+					.stroke(color, lineWidth: width)
+					.offset(x: finalX * width * 0.6, y: finalY * width * 0.6)
+					.blur(radius: blur)
+					.mask(shape)
+			)
+	}
+	
 }
