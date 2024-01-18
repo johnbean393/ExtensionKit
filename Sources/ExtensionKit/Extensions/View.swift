541f2dac16c8b8aa466ebed604f642e801b50c45
johnbean393 <johnbean393@gmail.com>
2024-01-18 11:13:08 +0800

    Update View.swift

--- Sources/ExtensionKit/Extensions/View.swift
+++ Sources/ExtensionKit/Extensions/View.swift
@@ -17,5 +17,11 @@ extension View {
 		}
 	}
 	
+	func glow(color: Color = .red, radius: CGFloat = 20) -> some View {
+		self
+			.shadow(color: color, radius: radius / 3)
+			.shadow(color: color, radius: radius / 3)
+			.shadow(color: color, radius: radius / 3)
+	}
+	
 }
-
