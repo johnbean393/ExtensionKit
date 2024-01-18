132423531d734d1c4d16d259cd2ee2255ffcc54e
johnbean393 <johnbean393@gmail.com>
2024-01-18 11:31:09 +0800

    Update View.swift

--- Sources/ExtensionKit/Extensions/View.swift
+++ Sources/ExtensionKit/Extensions/View.swift
@@ -19,6 +19,7 @@ extension View {
 	
 	public func glow(color: Color = .red, radius: CGFloat = 20) -> some View {
 		self
+			.overlay(self.blur(radius: radius / 6))
 			.shadow(color: color, radius: radius / 3)
 			.shadow(color: color, radius: radius / 3)
 			.shadow(color: color, radius: radius / 3)
