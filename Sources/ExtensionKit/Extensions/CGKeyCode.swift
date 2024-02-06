a5f32e7fc2778cc3f346e97af2cca023807ab20b
johnbean393 <johnbean393@gmail.com>
2024-02-07 00:10:20 +0800

    Added DBSCAN clustering

--- Sources/ExtensionKit/Extensions/CGKeyCode.swift
+++ Sources/ExtensionKit/Extensions/CGKeyCode.swift
@@ -29,137 +29,137 @@ extension CGKeyCode
 	 *    keycode.
 	 */
 	
-	static let kVK_ANSI_A                    : CGKeyCode = 0x00
-	static let kVK_ANSI_S                    : CGKeyCode = 0x01
-	static let kVK_ANSI_D                    : CGKeyCode = 0x02
-	static let kVK_ANSI_F                    : CGKeyCode = 0x03
-	static let kVK_ANSI_H                    : CGKeyCode = 0x04
-	static let kVK_ANSI_G                    : CGKeyCode = 0x05
-	static let kVK_ANSI_Z                    : CGKeyCode = 0x06
-	static let kVK_ANSI_X                    : CGKeyCode = 0x07
-	static let kVK_ANSI_C                    : CGKeyCode = 0x08
-	static let kVK_ANSI_V                    : CGKeyCode = 0x09
-	static let kVK_ANSI_B                    : CGKeyCode = 0x0B
-	static let kVK_ANSI_Q                    : CGKeyCode = 0x0C
-	static let kVK_ANSI_W                    : CGKeyCode = 0x0D
-	static let kVK_ANSI_E                    : CGKeyCode = 0x0E
-	static let kVK_ANSI_R                    : CGKeyCode = 0x0F
-	static let kVK_ANSI_Y                    : CGKeyCode = 0x10
-	static let kVK_ANSI_T                    : CGKeyCode = 0x11
-	static let kVK_ANSI_1                    : CGKeyCode = 0x12
-	static let kVK_ANSI_2                    : CGKeyCode = 0x13
-	static let kVK_ANSI_3                    : CGKeyCode = 0x14
-	static let kVK_ANSI_4                    : CGKeyCode = 0x15
-	static let kVK_ANSI_6                    : CGKeyCode = 0x16
-	static let kVK_ANSI_5                    : CGKeyCode = 0x17
-	static let kVK_ANSI_Equal                : CGKeyCode = 0x18
-	static let kVK_ANSI_9                    : CGKeyCode = 0x19
-	static let kVK_ANSI_7                    : CGKeyCode = 0x1A
-	static let kVK_ANSI_Minus                : CGKeyCode = 0x1B
-	static let kVK_ANSI_8                    : CGKeyCode = 0x1C
-	static let kVK_ANSI_0                    : CGKeyCode = 0x1D
-	static let kVK_ANSI_RightBracket         : CGKeyCode = 0x1E
-	static let kVK_ANSI_O                    : CGKeyCode = 0x1F
-	static let kVK_ANSI_U                    : CGKeyCode = 0x20
-	static let kVK_ANSI_LeftBracket          : CGKeyCode = 0x21
-	static let kVK_ANSI_I                    : CGKeyCode = 0x22
-	static let kVK_ANSI_P                    : CGKeyCode = 0x23
-	static let kVK_ANSI_L                    : CGKeyCode = 0x25
-	static let kVK_ANSI_J                    : CGKeyCode = 0x26
-	static let kVK_ANSI_Quote                : CGKeyCode = 0x27
-	static let kVK_ANSI_K                    : CGKeyCode = 0x28
-	static let kVK_ANSI_Semicolon            : CGKeyCode = 0x29
-	static let kVK_ANSI_Backslash            : CGKeyCode = 0x2A
-	static let kVK_ANSI_Comma                : CGKeyCode = 0x2B
-	static let kVK_ANSI_Slash                : CGKeyCode = 0x2C
-	static let kVK_ANSI_N                    : CGKeyCode = 0x2D
-	static let kVK_ANSI_M                    : CGKeyCode = 0x2E
-	static let kVK_ANSI_Period               : CGKeyCode = 0x2F
-	static let kVK_ANSI_Grave                : CGKeyCode = 0x32
-	static let kVK_ANSI_KeypadDecimal        : CGKeyCode = 0x41
-	static let kVK_ANSI_KeypadMultiply       : CGKeyCode = 0x43
-	static let kVK_ANSI_KeypadPlus           : CGKeyCode = 0x45
-	static let kVK_ANSI_KeypadClear          : CGKeyCode = 0x47
-	static let kVK_ANSI_KeypadDivide         : CGKeyCode = 0x4B
-	static let kVK_ANSI_KeypadEnter          : CGKeyCode = 0x4C
-	static let kVK_ANSI_KeypadMinus          : CGKeyCode = 0x4E
-	static let kVK_ANSI_KeypadEquals         : CGKeyCode = 0x51
-	static let kVK_ANSI_Keypad0              : CGKeyCode = 0x52
-	static let kVK_ANSI_Keypad1              : CGKeyCode = 0x53
-	static let kVK_ANSI_Keypad2              : CGKeyCode = 0x54
-	static let kVK_ANSI_Keypad3              : CGKeyCode = 0x55
-	static let kVK_ANSI_Keypad4              : CGKeyCode = 0x56
-	static let kVK_ANSI_Keypad5              : CGKeyCode = 0x57
-	static let kVK_ANSI_Keypad6              : CGKeyCode = 0x58
-	static let kVK_ANSI_Keypad7              : CGKeyCode = 0x59
-	static let kVK_ANSI_Keypad8              : CGKeyCode = 0x5B
-	static let kVK_ANSI_Keypad9              : CGKeyCode = 0x5C
+	static public let kVK_ANSI_A                    : CGKeyCode = 0x00
+	static public let kVK_ANSI_S                    : CGKeyCode = 0x01
+	static public let kVK_ANSI_D                    : CGKeyCode = 0x02
+	static public let kVK_ANSI_F                    : CGKeyCode = 0x03
+	static public let kVK_ANSI_H                    : CGKeyCode = 0x04
+	static public let kVK_ANSI_G                    : CGKeyCode = 0x05
+	static public let kVK_ANSI_Z                    : CGKeyCode = 0x06
+	static public let kVK_ANSI_X                    : CGKeyCode = 0x07
+	static public let kVK_ANSI_C                    : CGKeyCode = 0x08
+	static public let kVK_ANSI_V                    : CGKeyCode = 0x09
+	static public let kVK_ANSI_B                    : CGKeyCode = 0x0B
+	static public let kVK_ANSI_Q                    : CGKeyCode = 0x0C
+	static public let kVK_ANSI_W                    : CGKeyCode = 0x0D
+	static public let kVK_ANSI_E                    : CGKeyCode = 0x0E
+	static public let kVK_ANSI_R                    : CGKeyCode = 0x0F
+	static public let kVK_ANSI_Y                    : CGKeyCode = 0x10
+	static public let kVK_ANSI_T                    : CGKeyCode = 0x11
+	static public let kVK_ANSI_1                    : CGKeyCode = 0x12
+	static public let kVK_ANSI_2                    : CGKeyCode = 0x13
+	static public let kVK_ANSI_3                    : CGKeyCode = 0x14
+	static public let kVK_ANSI_4                    : CGKeyCode = 0x15
+	static public let kVK_ANSI_6                    : CGKeyCode = 0x16
+	static public let kVK_ANSI_5                    : CGKeyCode = 0x17
+	static public let kVK_ANSI_Equal                : CGKeyCode = 0x18
+	static public let kVK_ANSI_9                    : CGKeyCode = 0x19
+	static public let kVK_ANSI_7                    : CGKeyCode = 0x1A
+	static public let kVK_ANSI_Minus                : CGKeyCode = 0x1B
+	static public let kVK_ANSI_8                    : CGKeyCode = 0x1C
+	static public let kVK_ANSI_0                    : CGKeyCode = 0x1D
+	static public let kVK_ANSI_RightBracket         : CGKeyCode = 0x1E
+	static public let kVK_ANSI_O                    : CGKeyCode = 0x1F
+	static public let kVK_ANSI_U                    : CGKeyCode = 0x20
+	static public let kVK_ANSI_LeftBracket          : CGKeyCode = 0x21
+	static public let kVK_ANSI_I                    : CGKeyCode = 0x22
+	static public let kVK_ANSI_P                    : CGKeyCode = 0x23
+	static public let kVK_ANSI_L                    : CGKeyCode = 0x25
+	static public let kVK_ANSI_J                    : CGKeyCode = 0x26
+	static public let kVK_ANSI_Quote                : CGKeyCode = 0x27
+	static public let kVK_ANSI_K                    : CGKeyCode = 0x28
+	static public let kVK_ANSI_Semicolon            : CGKeyCode = 0x29
+	static public let kVK_ANSI_Backslash            : CGKeyCode = 0x2A
+	static public let kVK_ANSI_Comma                : CGKeyCode = 0x2B
+	static public let kVK_ANSI_Slash                : CGKeyCode = 0x2C
+	static public let kVK_ANSI_N                    : CGKeyCode = 0x2D
+	static public let kVK_ANSI_M                    : CGKeyCode = 0x2E
+	static public let kVK_ANSI_Period               : CGKeyCode = 0x2F
+	static public let kVK_ANSI_Grave                : CGKeyCode = 0x32
+	static public let kVK_ANSI_KeypadDecimal        : CGKeyCode = 0x41
+	static public let kVK_ANSI_KeypadMultiply       : CGKeyCode = 0x43
+	static public let kVK_ANSI_KeypadPlus           : CGKeyCode = 0x45
+	static public let kVK_ANSI_KeypadClear          : CGKeyCode = 0x47
+	static public let kVK_ANSI_KeypadDivide         : CGKeyCode = 0x4B
+	static public let kVK_ANSI_KeypadEnter          : CGKeyCode = 0x4C
+	static public let kVK_ANSI_KeypadMinus          : CGKeyCode = 0x4E
+	static public let kVK_ANSI_KeypadEquals         : CGKeyCode = 0x51
+	static public let kVK_ANSI_Keypad0              : CGKeyCode = 0x52
+	static public let kVK_ANSI_Keypad1              : CGKeyCode = 0x53
+	static public let kVK_ANSI_Keypad2              : CGKeyCode = 0x54
+	static public let kVK_ANSI_Keypad3              : CGKeyCode = 0x55
+	static public let kVK_ANSI_Keypad4              : CGKeyCode = 0x56
+	static public let kVK_ANSI_Keypad5              : CGKeyCode = 0x57
+	static public let kVK_ANSI_Keypad6              : CGKeyCode = 0x58
+	static public let kVK_ANSI_Keypad7              : CGKeyCode = 0x59
+	static public let kVK_ANSI_Keypad8              : CGKeyCode = 0x5B
+	static public let kVK_ANSI_Keypad9              : CGKeyCode = 0x5C
 	
 	// keycodes for keys that are independent of keyboard layout
-	static let kVK_Return                    : CGKeyCode = 0x24
-	static let kVK_Tab                       : CGKeyCode = 0x30
-	static let kVK_Space                     : CGKeyCode = 0x31
-	static let kVK_Delete                    : CGKeyCode = 0x33
-	static let kVK_Escape                    : CGKeyCode = 0x35
-	static let kVK_Command                   : CGKeyCode = 0x37
-	static let kVK_Shift                     : CGKeyCode = 0x38
-	static let kVK_CapsLock                  : CGKeyCode = 0x39
-	static let kVK_Option                    : CGKeyCode = 0x3A
-	static let kVK_Control                   : CGKeyCode = 0x3B
-	static let kVK_RightCommand              : CGKeyCode = 0x36 // Out of order
-	static let kVK_RightShift                : CGKeyCode = 0x3C
-	static let kVK_RightOption               : CGKeyCode = 0x3D
-	static let kVK_RightControl              : CGKeyCode = 0x3E
-	static let kVK_Function                  : CGKeyCode = 0x3F
-	static let kVK_F17                       : CGKeyCode = 0x40
-	static let kVK_VolumeUp                  : CGKeyCode = 0x48
-	static let kVK_VolumeDown                : CGKeyCode = 0x49
-	static let kVK_Mute                      : CGKeyCode = 0x4A
-	static let kVK_F18                       : CGKeyCode = 0x4F
-	static let kVK_F19                       : CGKeyCode = 0x50
-	static let kVK_F20                       : CGKeyCode = 0x5A
-	static let kVK_F5                        : CGKeyCode = 0x60
-	static let kVK_F6                        : CGKeyCode = 0x61
-	static let kVK_F7                        : CGKeyCode = 0x62
-	static let kVK_F3                        : CGKeyCode = 0x63
-	static let kVK_F8                        : CGKeyCode = 0x64
-	static let kVK_F9                        : CGKeyCode = 0x65
-	static let kVK_F11                       : CGKeyCode = 0x67
-	static let kVK_F13                       : CGKeyCode = 0x69
-	static let kVK_F16                       : CGKeyCode = 0x6A
-	static let kVK_F14                       : CGKeyCode = 0x6B
-	static let kVK_F10                       : CGKeyCode = 0x6D
-	static let kVK_F12                       : CGKeyCode = 0x6F
-	static let kVK_F15                       : CGKeyCode = 0x71
-	static let kVK_Help                      : CGKeyCode = 0x72
-	static let kVK_Home                      : CGKeyCode = 0x73
-	static let kVK_PageUp                    : CGKeyCode = 0x74
-	static let kVK_ForwardDelete             : CGKeyCode = 0x75
-	static let kVK_F4                        : CGKeyCode = 0x76
-	static let kVK_End                       : CGKeyCode = 0x77
-	static let kVK_F2                        : CGKeyCode = 0x78
-	static let kVK_PageDown                  : CGKeyCode = 0x79
-	static let kVK_F1                        : CGKeyCode = 0x7A
-	static let kVK_LeftArrow                 : CGKeyCode = 0x7B
-	static let kVK_RightArrow                : CGKeyCode = 0x7C
-	static let kVK_DownArrow                 : CGKeyCode = 0x7D
-	static let kVK_UpArrow                   : CGKeyCode = 0x7E
+	static public let kVK_Return                    : CGKeyCode = 0x24
+	static public let kVK_Tab                       : CGKeyCode = 0x30
+	static public let kVK_Space                     : CGKeyCode = 0x31
+	static public let kVK_Delete                    : CGKeyCode = 0x33
+	static public let kVK_Escape                    : CGKeyCode = 0x35
+	static public let kVK_Command                   : CGKeyCode = 0x37
+	static public let kVK_Shift                     : CGKeyCode = 0x38
+	static public let kVK_CapsLock                  : CGKeyCode = 0x39
+	static public let kVK_Option                    : CGKeyCode = 0x3A
+	static public let kVK_Control                   : CGKeyCode = 0x3B
+	static public let kVK_RightCommand              : CGKeyCode = 0x36 // Out of order
+	static public let kVK_RightShift                : CGKeyCode = 0x3C
+	static public let kVK_RightOption               : CGKeyCode = 0x3D
+	static public let kVK_RightControl              : CGKeyCode = 0x3E
+	static public let kVK_Function                  : CGKeyCode = 0x3F
+	static public let kVK_F17                       : CGKeyCode = 0x40
+	static public let kVK_VolumeUp                  : CGKeyCode = 0x48
+	static public let kVK_VolumeDown                : CGKeyCode = 0x49
+	static public let kVK_Mute                      : CGKeyCode = 0x4A
+	static public let kVK_F18                       : CGKeyCode = 0x4F
+	static public let kVK_F19                       : CGKeyCode = 0x50
+	static public let kVK_F20                       : CGKeyCode = 0x5A
+	static public let kVK_F5                        : CGKeyCode = 0x60
+	static public let kVK_F6                        : CGKeyCode = 0x61
+	static public let kVK_F7                        : CGKeyCode = 0x62
+	static public let kVK_F3                        : CGKeyCode = 0x63
+	static public let kVK_F8                        : CGKeyCode = 0x64
+	static public let kVK_F9                        : CGKeyCode = 0x65
+	static public let kVK_F11                       : CGKeyCode = 0x67
+	static public let kVK_F13                       : CGKeyCode = 0x69
+	static public let kVK_F16                       : CGKeyCode = 0x6A
+	static public let kVK_F14                       : CGKeyCode = 0x6B
+	static public let kVK_F10                       : CGKeyCode = 0x6D
+	static public let kVK_F12                       : CGKeyCode = 0x6F
+	static public let kVK_F15                       : CGKeyCode = 0x71
+	static public let kVK_Help                      : CGKeyCode = 0x72
+	static public let kVK_Home                      : CGKeyCode = 0x73
+	static public let kVK_PageUp                    : CGKeyCode = 0x74
+	static public let kVK_ForwardDelete             : CGKeyCode = 0x75
+	static public let kVK_F4                        : CGKeyCode = 0x76
+	static public let kVK_End                       : CGKeyCode = 0x77
+	static public let kVK_F2                        : CGKeyCode = 0x78
+	static public let kVK_PageDown                  : CGKeyCode = 0x79
+	static public let kVK_F1                        : CGKeyCode = 0x7A
+	static public let kVK_LeftArrow                 : CGKeyCode = 0x7B
+	static public let kVK_RightArrow                : CGKeyCode = 0x7C
+	static public let kVK_DownArrow                 : CGKeyCode = 0x7D
+	static public let kVK_UpArrow                   : CGKeyCode = 0x7E
 	
 	// ISO keyboards only
-	static let kVK_ISO_Section               : CGKeyCode = 0x0A
+	static public let kVK_ISO_Section               : CGKeyCode = 0x0A
 	
 	// JIS keyboards only
-	static let kVK_JIS_Yen                   : CGKeyCode = 0x5D
-	static let kVK_JIS_Underscore            : CGKeyCode = 0x5E
-	static let kVK_JIS_KeypadComma           : CGKeyCode = 0x5F
-	static let kVK_JIS_Eisu                  : CGKeyCode = 0x66
-	static let kVK_JIS_Kana                  : CGKeyCode = 0x68
+	static public let kVK_JIS_Yen                   : CGKeyCode = 0x5D
+	static public let kVK_JIS_Underscore            : CGKeyCode = 0x5E
+	static public let kVK_JIS_KeypadComma           : CGKeyCode = 0x5F
+	static public let kVK_JIS_Eisu                  : CGKeyCode = 0x66
+	static public let kVK_JIS_Kana                  : CGKeyCode = 0x68
 	
-	var isModifier: Bool {
+	public var isModifier: Bool {
 		return (.kVK_RightCommand...(.kVK_Function)).contains(self)
 	}
 	
-	var baseModifier: CGKeyCode?
+	public var baseModifier: CGKeyCode?
 	{
 		if (.kVK_Command...(.kVK_Control)).contains(self)
 			|| self == .kVK_Function
@@ -178,7 +178,7 @@ extension CGKeyCode
 		}
 	}
 	
-	var isPressed: Bool {
+	public var isPressed: Bool {
 		CGEventSource.keyState(.combinedSessionState, key: self)
 	}
 }
---
+++ Sources/ExtensionKit/Tools/DBSCAN.swift
@@ -0,0 +1,120 @@
+//
+//  DBSCAN.swift
+//
+//
+//  Created by Bean John on 7/2/2024.
+//
+
+import Foundation
+
+public class DBSCAN<Value: Hashable> {
+	
+	public var data: [Value]
+	public var label: [Value: String]
+	
+	public init(data: [Value])  {
+		
+		self.data = data
+		self.label = [Value: String]()
+		for point in self.data {
+			self.label[point] = "undefined"
+		}
+		
+	}
+	
+	public func cluster(distance: (Value, Value) -> Double, epsilon: Double, minPoints: Int) {
+		
+		var count: Int = 0
+		
+		for point in self.data {
+			
+			if self.label[point] != "undefined" {
+				continue
+			}
+			
+			var neighbours: [Value] = self.rangeQuery(distance: distance, neighbour: point, epsilon: epsilon)
+			
+			if neighbours.count < minPoints  {
+				
+				self.label[point] = "noise"
+				continue
+			}
+			
+			count += 1
+			
+			self.label[point] = "\(count)"
+			
+			for neighbour in neighbours {
+				
+				if self.label[neighbour] == "noise" {
+					self.label[neighbour] = "\(count)"
+				}
+				
+				if self.label[neighbour] != "undefined" {
+					continue
+				}
+				
+				label[neighbour] = "\(count)"
+				
+				let furtherNeighbours: [Value] = self.rangeQuery(distance: distance, neighbour: neighbour, epsilon: epsilon)
+				
+				if furtherNeighbours.count >= minPoints  {
+					neighbours.append(contentsOf:  furtherNeighbours)
+					continue
+				}
+			}
+		}
+	}
+	
+	private func rangeQuery(distance: (Value, Value) -> Double, neighbour: Value, epsilon: Double) -> [Value] {
+		
+		var neighbors = [Value]()
+		for point in self.data {
+			if distance(neighbour, point) <= epsilon {
+				neighbors.append(point)
+			}
+		}
+		return neighbors
+		
+	}
+	
+}
+
+// Example use
+
+//let input: [[Double]] = [
+//	[0, 10, 20],
+//	[0, 11, 21],
+//	[0, 12, 20],
+//	[20, 33, 59],
+//	[21, 32, 56],
+//	[59, 77, 101],
+//	[58, 79, 100],
+//	[58, 76, 102],
+//	[300, 70, 20],
+//	[500, 300, 202],
+//	[500, 302, 204]
+//]
+
+// Define distance func
+//func euclideanDistance(first: [Double], second: [Double]) -> Double  {
+//	guard first.count == second.count else {
+//		fatalError("Vector space is not the same")
+//	}
+//	var result: Double = 0
+//	for i in 0..<first.count {
+//		result += pow(first[i] - second[i], 2.0)
+//	}
+//	return sqrt(result)
+//}
+
+// Init object
+//let dbScan: DBSCAN = DBSCAN(data: input)
+// Run cluster
+//dbScan.cluster(distance: euclideanDistance, epsilon: 10, minPoints: 2)
+
+// Print results
+//for (index, currData) in dbScan.data.enumerated() {
+//	print("\(currData) -> \(dbScan.label[currData] ?? "")")
+//}
+
