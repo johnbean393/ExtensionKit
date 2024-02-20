67467ca4ca265584c6ed8f7075149424a5998af4
johnbean393 <johnbean393@gmail.com>
2024-02-20 17:20:04 +0800

    Added string extensions

--- Sources/ExtensionKit/Extensions/String.swift
+++ Sources/ExtensionKit/Extensions/String.swift
@@ -341,4 +341,16 @@ extension String {
 		}
 	}
 	
+	public var lowercasingFirst: String { prefix(1).lowercased() + dropFirst() }
+	public var uppercasingFirst: String { prefix(1).uppercased() + dropFirst() }
+	
+	public var camelCased: String {
+		guard !isEmpty else { return "" }
+		let parts = components(separatedBy: .alphanumerics.inverted)
+		let first = parts.first!.lowercasingFirst
+		let rest = parts.dropFirst().map { $0.uppercasingFirst }
+		
+		return ([first] + rest).joined()
+	}
+	
 }
