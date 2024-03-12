af9167a9a1f6ad599d6aa98d5c9992eb3cee2274
johnbean393 <johnbean393@gmail.com>
2024-03-12 09:42:20 +0800

    Removed animation

--- Sources/ExtensionKit/Views/HoverEffectModifier.swift
+++ Sources/ExtensionKit/Views/HoverEffectModifier.swift
@@ -11,21 +11,19 @@ import SwiftUI
 extension View {
 	
 	public func glowOnHover(
-		animateIn: Double = 0.5,
 		color: Color = Color.accentColor,
 		radius: CGFloat = 10
 	) -> some View {
-		ModifiedContent(content: self, modifier: HoverEffectModifier(animateIn: animateIn, glowColor: color, glowRadius: radius))
+		ModifiedContent(content: self, modifier: HoverEffectModifier(glowColor: color, glowRadius: radius))
 	}
 	
 }
 
 struct HoverEffectModifier: ViewModifier {
 	
-	var animateIn: Double
 	var glowColor: Color
 	var glowRadius: CGFloat
-	@State private var isHovering: Bool = true
+	@State private var isHovering: Bool = false
 	
 	func body(content: Content) -> some View {
 		return Group {
@@ -35,20 +33,12 @@ struct HoverEffectModifier: ViewModifier {
 					.shadow(color: glowColor, radius: glowRadius / 3)
 					.shadow(color: glowColor, radius: glowRadius / 3)
 					.onHover { hovering in
-						if isHovering != hovering {
-							withAnimation(.smooth(duration: animateIn)) {
-								isHovering = hovering
-							}
-						}
+						isHovering = hovering
 					}
 			} else {
 				content
 					.onHover { hovering in
-						if isHovering != hovering {
-							withAnimation(.smooth(duration: animateIn)) {
-								isHovering = hovering
-							}
-						}
+						isHovering = hovering
 					}
 			}
 		}
