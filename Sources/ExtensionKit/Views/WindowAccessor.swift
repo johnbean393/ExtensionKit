e6c4c91e01488c8c02a78c048e177df2aaf06f6f
johnbean393 <johnbean393@gmail.com>
2024-03-14 15:18:42 +0800

    Make WindowAccessor public

--- Sources/ExtensionKit/Views/WindowAccessor.swift
+++ Sources/ExtensionKit/Views/WindowAccessor.swift
@@ -10,7 +10,7 @@ import Foundation
 import AppKit
 import SwiftUI
 
-struct WindowAccessor: NSViewRepresentable {
+public struct WindowAccessor: NSViewRepresentable {
 	
 	@Binding var window: NSWindow?
 	
