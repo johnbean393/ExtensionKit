59d700ddc416923b30ea24c6640f19f5861a8a6d
johnbean393 <johnbean393@gmail.com>
2024-03-14 12:14:17 +0800

    Changed XDR boost

--- Sources/ExtensionKit/Views/Modifiers/XDRBoostModifier.swift
+++ Sources/ExtensionKit/Views/Modifiers/XDRBoostModifier.swift
@@ -1,8 +1,39 @@
 //
-//  File.swift
-//  
+//  XDRBoostModifier.swift
+//
 //
 //  Created by Bean John on 14/3/2024.
 //
 
 import Foundation
+import SwiftUI
+
+extension View {
+	
+	public func xdrBoost() -> some View {
+		ModifiedContent(content: self, modifier: XDRBoostModifier())
+	}
+	
+}
+
+struct XDRBoostModifier: ViewModifier {
+	
+	@State private var nsWindow: NSWindow?
+	
+	func body(content: Content) -> some View {
+		return content
+			.background(WindowAccessor(window: $nsWindow))
+			.onAppear {
+				DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+					if let view = nsWindow?.contentView {
+						// The MTKView instance
+						var metalView: MetalView!
+						metalView = MetalView(frame: view.bounds, frameRate: 1, contrast: 1.0, brightness: 1.0)
+						metalView.autoresizingMask = [.width, .height]
+						view.addSubview(metalView)
+					}
+				}
+			}
+	}
+	
+}
--- Sources/ExtensionKit/Views/WindowAccessor.swift
+++ Sources/ExtensionKit/Views/WindowAccessor.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
-//  
+//  WindowAccessor.swift
+//
 //
 //  Created by Bean John on 14/3/2024.
 //
 
 import Foundation
+import AppKit
+import SwiftUI
+
+struct WindowAccessor: NSViewRepresentable {
+	
+	@Binding var window: NSWindow?
+	
+	func makeNSView(context: Context) -> NSView {
+		let view = NSView()
+		DispatchQueue.main.async {
+			self.window = view.window
+		}
+		return view
+	}
+	
+	func updateNSView(_ nsView: NSView, context: Context) {}
+}
