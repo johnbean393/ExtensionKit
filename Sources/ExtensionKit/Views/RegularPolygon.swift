ef302b0a8b011227839e046a928bcadaab6c47c5
johnbean393 <johnbean393@gmail.com>
2024-03-29 10:03:54 +0800

    Added RegularPolygon struct

---
+++ Sources/ExtensionKit/Views/RegularPolygon.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Bean John on 29/3/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
