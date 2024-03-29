91d6012eeb25c568380ca19024b36e86603746b3
johnbean393 <johnbean393@gmail.com>
2024-03-29 10:04:43 +0800

    Made path method public

--- Sources/ExtensionKit/Views/RegularPolygon.swift
+++ Sources/ExtensionKit/Views/RegularPolygon.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    SwiftUIView()
+public struct RegularPolygon: Shape {
+	
+	var sides: Int
+	
+	public func path(in rect: CGRect) -> Path {
+		var path: Path = Path()
+		let limitingEdge: CGFloat = min(rect.width, rect.height)
+		var radius: CGFloat = limitingEdge / 2
+		let center: CGPoint = CGPoint(
+			x: rect.midX,
+			y: rect.midY
+		)
+		let angles: [Angle] = Array(1...sides).map({ Angle(degrees: Double($0) * 360.0 / Double(sides) + 180) })
+		let rawPoints: [CGPoint] = angles.map { angle in
+			let xOffset: CGFloat = sin(angle.radians) * radius
+			let yOffset: CGFloat = cos(angle.radians) * radius
+			return CGPoint(x: xOffset + center.x, y: yOffset + center.y)
+		}
+		let lowestY: CGFloat = rawPoints.map({ $0.y }).max()!
+		let yOffset: CGFloat = (rect.maxY - lowestY) / 2
+		let points: [CGPoint] = rawPoints.map { point in
+			return CGPoint(x: point.x, y: point.y + yOffset)
+		}
+		path.move(to: points.first!)
+		for (index, point) in Array(points.dropFirst()).enumerated() {
+			path.addLine(to: point)
+		}
+		path.closeSubpath()
+		return path
+	}
+	
 }
