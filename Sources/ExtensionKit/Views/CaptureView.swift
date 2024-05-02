0c3bf3757012c2827bed7f049fc635a5710c8dea
johnbean393 <johnbean393@gmail.com>
2024-05-02 12:13:02 +0800

    Added CaptureView.swift

---
+++ Sources/ExtensionKit/Views/CaptureView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Bean John on 2/5/2024.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+	
+	public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+	
+}
+
+#Preview {
+    SwiftUIView()
+}
