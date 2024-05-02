b17927f084557f7f654387cd0f256bcbe7597713
johnbean393 <johnbean393@gmail.com>
2024-05-02 12:13:22 +0800

    Added CaptureView.swift

--- Sources/ExtensionKit/Views/CaptureView.swift
+++ Sources/ExtensionKit/Views/CaptureView.swift
@@ -1,20 +1,50 @@
 //
-//  SwiftUIView.swift
-//  
+//  CaptureView.swift
+//
 //
 //  Created by Bean John on 2/5/2024.
 //
 
 import SwiftUI
 
-public struct SwiftUIView: View {
+#if os(macOS)
+@available(macOS 13.0, *)
+public struct CaptureView<Content: View>: View {
+
+	@Binding var productDir: URL
+	@Binding var filename: String
+	var content: () -> Content
 	
 	public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+		VStack {
+			VStack(content: content)
+			Button("Render") {
+				Task {
+					// Correct filename if needed
+					if !filename.hasSuffix(".png") {
+						filename = filename + ".png"
+					}
+					// Render and save image
+					let renderer: ImageRenderer = ImageRenderer(content: VStack(content: content))
+					if let image = renderer.nsImage {
+						print(image.size)
+						saveNSImage(filepath: productDir.posixPath() + filename, image: image)
+					}
+				}
+			}
+		}
+	}
+	
+	func saveNSImage(filepath: String, image: NSImage) {
+		let imageRepresentation = NSBitmapImageRep(data: image.tiffRepresentation!)
+		let pngData = imageRepresentation?.representation(using: .png, properties: [:])
+		do {
+			try pngData!.write(to: URL(fileURLWithPath: filepath))
+		} catch {
+			print(error)
+		}
+	}
 	
 }
 
-#Preview {
-    SwiftUIView()
-}
+#endif
